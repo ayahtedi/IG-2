7c366981716838c5892361be51d4b63cc1722587
ayahtedi <122600655+ayahtedi@users.noreply.github.com>
2023-01-21 16:44:19 +0700

    Create bodas.php

---
+++ bodas.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Jasa Sosial Media</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 1000px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+    <br><br><br><br>
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    <center><img src="https://raw.githubusercontent.com/ayahtedi/IG-1/main/images.png" class="img-fluid" alt="Responsive image">
+  </div>
+  <div class="container">
+    <center><a href="https://tomidigital.id/ig/cs.php" target="_blank"><img src="https://raw.githubusercontent.com/ayahtedi/IG-1/main/WA.gif" class="img-fluid" alt="Responsive image"></a>
+  </div>
+</main>
+
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+    <span class="text-muted"><center>Made with ❤️ Tomipages</span>
+  </div>
+</footer>
+
+
+    
+  </body>
+</html>
+  <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4275232,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4275232&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
